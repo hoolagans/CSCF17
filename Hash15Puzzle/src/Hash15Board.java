77ab190ab75f6a889e6af552f371ebe1d180566d
pizza <pizzahaut@gmail.com'>
2017-09-29 11:17:41 -0400

    commit

---
+++ Hash15Puzzle/src/Hash15Board.java
@@ -0,0 +1,15 @@
+/**
+ *Hash15Board.java -- hasable boards to store positions in solution of 15 puzzle and create lists
+ *of available moves, etc.
+ *@author Nathan Haut
+ *9/29/17 
+ *
+ */
+public class Hash15Board {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ Hash15Puzzle/src/Hash15Driver.java
@@ -0,0 +1,12 @@
+/*
+ * Hash14Driver.java--code to run 15 puzzle solver
+ * 
+ */
+public class Hash15Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
