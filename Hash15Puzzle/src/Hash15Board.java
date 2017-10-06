0c8f2313fa20551c5a257f259e2d0befae90457f
pizza <pizzahaut@gmail.com'>
2017-10-06 10:56:19 -0400

    added transitions

--- Hash15Puzzle/src/Hash15Board.java
+++ Hash15Puzzle/src/Hash15Board.java
@@ -1,12 +1,28 @@
 /**
- *Hash15Board.java -- hasable boards to store positions in solution of 15 puzzle and create lists
+ *Hash15Board.java -- hashable boards to store positions in solution of 15 puzzle and create lists
  *of available moves, etc.
  *@author Nathan Haut
  *9/29/17 
  *
  */
 public class Hash15Board {
-
+	private static final int [][] transitions
+	private static final int [][] legalMoves(int zeroPos){
+		
+		return transitions[zeroPos];
+	}
+	//Need to override hashCode of parent
+	public int hashCode(){
+		
+		return 0;
+	}
+	public boolean equals(Hash15Board a, Hash15Board b){
+		
+		
+		return true;
+		
+	}
+	public void 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- Hash15Puzzle/src/Hash15Driver.java
+++ Hash15Puzzle/src/Hash15Driver.java
@@ -1,12 +1,15 @@
 /*
  * Hash14Driver.java--code to run 15 puzzle solver
- * 
+ * Author  Nathan Haut
+ * Class CSC 230
  */
-public class Hash15Driver {
 
+public class Hash15Driver {
+	Set hset;
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+     hset = new HashSet();
+     
 	}
 
 }
